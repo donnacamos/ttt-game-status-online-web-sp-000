2943951cd4f9fdf858692e5b6498d3a5380d1a93
Donna Amos <donnacamos.developer@gmail.com>
2019-03-01 03:39:06 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -19,7 +19,9 @@ WIN_COMBINATIONS = [
   
  def won?(board)
     WIN_COMBINATIONS.each {|win| 
-    index_0 = 
+    index_0 = win_combo[0]
+    index_1 = win_combo[1]
+    index_2 = win_combo[2]
       
       
       
